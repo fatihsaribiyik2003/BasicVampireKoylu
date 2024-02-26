7df280fe16a31640bb22778b2a723a9f651cb0c4
fatihsaribiyik2003 <fatihsaribiyik2003@gmail.com>
2024-02-26 22:06:07 +0300

    BasicVampireKöylü
    ilk iki sayfa yapıldı birisi yarım

---
+++ Assets/Scripts/BeginMenü.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BeginMenü : MonoBehaviour
+{
+    // Bu fonksiyon Play tuşuna basıldığında çağrılacak
+    public void LoadGameplayScene()
+    {
+        // "Gameplay" sahnesine geçiş yap
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+    
+}
